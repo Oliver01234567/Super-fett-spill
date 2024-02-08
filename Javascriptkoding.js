9fa597fbbe1493ade5f699e4bc833611096239b8
Olav45 <oivarrud@gmail.com>
2024-02-08 09:48:21 +0100

    Bevegelse

--- Javascriptkoding.js
+++ Javascriptkoding.js
@@ -1,40 +1,61 @@
-//X og Y koordinater
-let y = 8
-let x = 1
 
 
-function BevegelseFrammover() {
+//Bevegelse 
 
-    x += 1
-    document.getElementById("character").style.gridColumn = x
-    document.getElementById("character").style.transform = "scaleX(1)"
-}
-
-
-function BevegelseOppover() {
-    y -= 1;
-    document.getElementById("character").style.gridRow = y;
-}
-
-function BevegelseNedover() {
-    y += 1;
-    document.getElementById("character").style.gridRow = y;
-}
-
-function BevegelseBakover() {
-    x -= 1;
-    document.getElementById("character").style.gridColumn = x
-    document.getElementById("character").style.transform = "scaleX(-1)"
-}
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowRight") {
-        BevegelseFrammover();
-    } else if (event.key === "ArrowDown") {
-        BevegelseNedover();
-    } else if (event.key === "ArrowLeft") {
-        BevegelseBakover();
-    } else if (event.key === "ArrowUp") {
-        BevegelseOppover();
+    
+document.addEventListener("keydown", function(event) {
+    if (event.key === "d") {
+      moveLeft(); 
+      console.log("d pressed")
     }
-});
+  });
+
+  function moveLeft() {
+    var character = document.getElementById("character");
+    var currentLeft = parseInt(character.style.left) || 0; 
+    character.style.left = (currentLeft + 100) + "px"; 
+    character.style.backgroundColor ="red"
+  }
+  
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "a") {
+      moveRight(); 
+      console.log("a pressed")
+    }
+  });
+
+  function moveRight() {
+    var character = document.getElementById("character");
+    var currentLeft = parseInt(character.style.left) || 0; 
+    character.style.left = (currentLeft + 100) + "px";
+    character.style.backgroundColor ="green"
+  }
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "s") {
+      moveDown(); 
+      console.log("s pressed")
+    }
+  });
+
+  function moveDown() {
+    var character = document.getElementById("character");
+    var currentTop = parseInt(character.style.top) || 0; 
+    character.style.top = (currentTop + 100) + "px"; 
+    character.style.backgroundColor ="blue"
+  }
+
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "w") {
+      moveUp(); 
+      console.log("w pressed")
+    }
+  });
+
+  function moveUp() {
+    var character = document.getElementById("character");
+    var currentTop = parseInt(character.style.top) || 0; 
+    character.style.top = (currentTop - 100) + "px"; 
+    character.style.backgroundColor ="orange"
+  }
