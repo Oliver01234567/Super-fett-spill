4e83841a9cf833364931fdb94b3e4330308a4f8b
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-02-21 22:19:54 +0100

    du kan nå velge hvem du vil fighte i arenaen, pluss Jonas er i spillet nå

--- Javascriptkoding.js
+++ Javascriptkoding.js
@@ -7,6 +7,8 @@ const characterP = document.querySelector("#character img")
 let choosenSkin = 0
 choosenSkin = parseInt(localStorage.getItem("choosenSkin")) || 0
 
+//Rasmus er 0
+//Anden er 1
 
 
 if (choosenSkin == 0) {
@@ -17,13 +19,33 @@ if (choosenSkin == 1) {
   characterP.src = "Bilder/playerIcon1.png"
 }
 
-let opponent = 0
 
-function fightRasmus() {
-  opponent = 1
+//variabel for fiender
+let opponent = 0
+const enemy0 = document.getElementById("enemy0")
+enemy0.addEventListener("click", fightAnden)
+
+const enemy1 = document.getElementById("enemy1")
+enemy1.addEventListener("click", fightJonas)
+
+//de ulike fiendene
+function fightAnden() {
+  opponent = 0
   localStorage.setItem("opponent", opponent);
 
 }
+
+function fightJonas() {
+  opponent = 1
+  localStorage.setItem("opponent", opponent)
+}
+
+
+
+//Anden er 0
+//Jonas er 1
+
+
 
 
 //Bevegelse 
