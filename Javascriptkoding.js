44d58f31a7ff53b52ecbc6ff183b7b1461fe40fb
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-02-27 11:31:14 +0100

    endra noen farger

--- Javascriptkoding.js
+++ Javascriptkoding.js
@@ -94,17 +94,17 @@ function SpeedIncrease() {
 }
 function speedNivå() {
   if (movePlayer === 6) {
-    speed1.style.backgroundColor = "red"
+    speed1.style.backgroundColor = "blue"
     showAlert("Fart oppgradert for " + upgradeSpeed + " penger. Ny fart: " + movePlayer, "success")
     speedUpg.innerText = formatNumber(upgradeSpeed);
   }
   if (movePlayer === 8) {
-    speed2.style.backgroundColor = "red"
+    speed2.style.backgroundColor = "blue"
     showAlert("Fart oppgradert for " + upgradeSpeed + " penger. Ny fart: " + movePlayer, "success")
     speedUpg.innerText = formatNumber(upgradeSpeed);
   }
   if (movePlayer === 10) {
-    speed3.style.backgroundColor = "red"
+    speed3.style.backgroundColor = "blue"
     showAlert("Fart oppgradert for " + upgradeSpeed + " penger. Ny fart: " + movePlayer, "success")
     const speedButton = document.getElementById("speedButton")
     speedUpg.innerText = formatNumber(upgradeSpeed);
@@ -166,19 +166,19 @@ function healthLevelIndicator() {
     healthButton.removeEventListener("click", healthIncrease)
   }
   if (plyHealth >= 33) {
-    health1.style.backgroundColor = "red"
+    health1.style.backgroundColor = "green"
   }
   if (plyHealth >= 36) {
-    health2.style.backgroundColor = "red"
+    health2.style.backgroundColor = "green"
   }
   if (plyHealth >= 39) {
-    health3.style.backgroundColor = "red"
+    health3.style.backgroundColor = "green"
   }
   if (plyHealth >= 42) {
-    health4.style.backgroundColor = "red"
+    health4.style.backgroundColor = "green"
   }
   if (plyHealth >= 45) {
-    health5.style.backgroundColor = "red"
+    health5.style.backgroundColor = "green"
   }
 
 }
