84adb1f6cd5000d01a9eef268fe52f365c05b833
ElineWr <elinewr@viken.no>
2024-02-27 13:57:36 +0100

    laget quiz

---
+++ Quiz/quiz.js
@@ -0,0 +1,34 @@
+
+
+quiz_poeng = 0
+
+
+
+function hente_resultater() {
+
+    svar1 = document.getElementById("riktig_svar1")
+    svar2 = document.getElementById("riktig_svar2")
+    svar3 = document.getElementById("riktig_svar3")
+    svar4 = document.getElementById("riktig_svar4")
+
+    if (svar1.checked == true) {
+        if (svar2.checked == true) {
+            if (svar3.checked == true) {
+                if (svar4.checked == true) {
+                    alert("Riktig")
+                    let quiz1Seier = 1
+                    localStorage.setItem("quiz1Seier", quiz1Seier)
+                } else {
+                    alert("wrong")
+                }
+            } else {
+                alert("wrong")
+            }
+        } else {
+            alert("wrong")
+        }
+    } else {
+        alert("wrong ")
+    }
+
+}
