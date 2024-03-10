0b40a7cedf4975db56d0df791d65767e7abdb9e5
AlexanderBack1 <158446023+AlexanderBack1@users.noreply.github.com>
2024-03-10 20:53:15 +0100

    update til chest åpnings animasjonen

--- Chests/ChestOpen.js
+++ Chests/ChestOpen.js
@@ -1,21 +1,30 @@
 const chest = document.getElementById("Chest")
 
+const main = document.getElementById("main")
+
+const openChestButton = document.getElementById("openChest")
+openChestButton.addEventListener("click", openChest)
+
+const chestDiv = document.getElementById("chestDiv")
+
 let quiz1Seier = localStorage.getItem("quiz1Seier") || 0
 
-let matteSeier = localStorage.getItem("matteSeier", matteSeier) || 0
+let matteSeier = localStorage.getItem("matteSeier") || 0
+
 
 function openChest() {
+    openChestButton.removeEventListener("click", openChest)
     chest.style.animation = ("openAnimation 2s infinite linear")
     setTimeout(removeAnimation, 2000)
     setTimeout(changeImg, 2100)
     setTimeout(createMoney, 2300)
 
-    if(quiz1Seier != 1 && matteSeier != 1) {
+    if (quiz1Seier != 1 && matteSeier != 1) {
         let getMoney = 1000
         localStorage.setItem("getMoney", getMoney)
     }
 
-    
+
 }
 
 function removeAnimation() {
@@ -30,10 +39,16 @@ function createMoney() {
     let money = document.createElement("img")
     money.src = "../Bilder/pengesekk.png"
     money.id = "money"
-    document.getElementById("body").appendChild(money)
+    chestDiv.appendChild(money)
+    spillavAHHHH()
     money.style.animation = ("moneyGrow 2s infinite linear")
-    setTimeout(function() {
-       window.location.href = '../Island/index.html';
-    }, 1800)
+    setTimeout(function () {
+      window.location.href = '../Island/index.html';
+   }, 1800)
 }
 
+function spillavAHHHH() {
+    let lydElement = document.getElementById('moneySOund');
+    lydElement.currentTime = 0;
+    lydElement.play();
+}
